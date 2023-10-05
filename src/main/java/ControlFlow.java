95f65ef0c24543fbad2862704772372181b436fa
Billie Redwood <billieredwood@gmail.com>
2023-10-05 16:20:14 +0100

    first commit

---
+++ src/main/java/ControlFlow.java
@@ -0,0 +1,22 @@
+public class ControlFlow {
+
+    public static void main(String[] args){
+//        System.out.println("Hello from ControlFlow");
+        System.out.println(5 >= 5);
+        System.out.println(4 > 9);
+        System.out.println(2 <= 3);
+        System.out.println(10 < 3);
+
+
+        System.out.println(5 == 3 + 2);
+        System.out.println(10 == 3 + 3);
+                System.out.println("cat".equals("cat"));
+
+
+    }
+
+
+
+
+
+}
